e6811b86f7dd7556f096405c8ad491ef6f85337d
zswalker <zswalker@my.waketech.edu>
2022-11-28 14:38:40 -0500

    basic outline

--- index.php
+++ index.php
@@ -3,14 +3,46 @@
 <head>
 	<meta charset="utf-8" />
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>Title</title>
+	<title>Car Lot Inventory</title>
 	<link href="style.css" type="text/css" rel="stylesheet" />
 </head>
 
 <body>
-  <?php
-    print "Initital Homepage Test";
-	print "Initital Homepage Test";
-  ?>
+<div class="container">
+	<header id="top">
+		<h1>Walker's Used Car Lot</h1>
+	</header>
+	<main>
+		<div id="about">
+			<nav>
+				<img src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.autoweek.com%2Fnews%2Ftechnology%2Fa40613374%2Fmost-popular-cars-on-tiktok%2F&psig=AOvVaw2H-XzmrVhIKylYnLHvuzS6&ust=1669745545348000&source=images&cd=vfe&ved=0CA8QjRxqFwoTCIiWl4K90fsCFQAAAAAdAAAAABAP" alt="Cars Image">
+				<h2>About</h2>
+				<p>This website is used for finding your next used car from our used car lot.</p>
+				<br><br><br><br><br><br />
+			</nav>
+		</div>
+		<section id="tools">
+			<h3>Tools</h3>
+			<table>
+				<thead>
+					<tr>
+						<th>Search/Add Vehicle</th>
+					</tr>
+				</thead>
+				<tbody>
+				<tr>
+					<td>
+						<ul>
+							<li> <a href="search-model.php">Search By Year, Make, and Model</a></li>
+							<li> <a href="search-budget.php">Search By Budget</a></li>
+							<li> <a href="search-style.php">Search By Style</a></li>
+							<li> <a href="add-vehicle.php">Add Vehicle</a></li>
+						</ul>
+					</td>
+				</tr>
+				</tbody>
+			</table>
+		</section>
+	</main>
 </body>
 </html>
