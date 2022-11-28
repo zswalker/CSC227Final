98135fca24b799b82f7ec03ab4e9c531476cb012
zswalker <zswalker@my.waketech.edu>
2022-11-28 16:32:07 -0500

    edit img width

--- index.php
+++ index.php
@@ -15,7 +15,7 @@
 	<main>
 		<div id="about">
 			<nav>
-				<img src="cars.jpg" alt="Cars Image" style="width:500px;height:600px;">
+				<img src="cars.jpg" alt="Cars Image" style="width:400px;height:350px;">
 				<h2>About</h2>
 				<p>This website is used for finding your next used car from our used car lot.</p>
 				<br><br><br><br><br><br />
