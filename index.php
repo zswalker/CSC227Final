4a310944aede7fb6a1adae4d364b6ab1e7736969
zswalker <zswalker@my.waketech.edu>
2022-11-28 15:03:07 -0500

    uncomment

--- index.php
+++ index.php
@@ -15,7 +15,7 @@
 	<main>
 		<div id="about">
 			<nav>
-				<!-- <img src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.autoweek.com%2Fnews%2Ftechnology%2Fa40613374%2Fmost-popular-cars-on-tiktok%2F&psig=AOvVaw2H-XzmrVhIKylYnLHvuzS6&ust=1669745545348000&source=images&cd=vfe&ved=0CA8QjRxqFwoTCIiWl4K90fsCFQAAAAAdAAAAABAP" alt="Cars Image"> -->
+				<img src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.autoweek.com%2Fnews%2Ftechnology%2Fa40613374%2Fmost-popular-cars-on-tiktok%2F&psig=AOvVaw2H-XzmrVhIKylYnLHvuzS6&ust=1669745545348000&source=images&cd=vfe&ved=0CA8QjRxqFwoTCIiWl4K90fsCFQAAAAAdAAAAABAP" alt="Cars Image">
 				<h2>About</h2>
 				<p>This website is used for finding your next used car from our used car lot.</p>
 				<br><br><br><br><br><br />
@@ -33,10 +33,10 @@
 				<tr>
 					<td>
 						<ul>
-							<!-- <li> <a href="search-model.php">Search By Year, Make, and Model</a></li>
+							<li> <a href="search-model.php">Search By Year, Make, and Model</a></li>
 							<li> <a href="search-budget.php">Search By Budget</a></li>
 							<li> <a href="search-style.php">Search By Style</a></li>
-							<li> <a href="add-vehicle.php">Add Vehicle</a></li> -->
+							<li> <a href="add-vehicle.php">Add Vehicle</a></li>
 						</ul>
 					</td>
 				</tr>
