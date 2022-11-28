e5d9d4235d13ccffb300bf649a419c6b5cd3ddec
zswalker <zswalker@my.waketech.edu>
2022-11-28 16:31:53 -0500

    initial search-model.php

---
+++ search-model.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8" />
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Car Lot Search</title>
+	<link href="style.css" type="text/css" rel="stylesheet" />
+</head>
+
+<body>
+    <?php
+    print "Initial Test"
+    ?>
+</body>
+</html>
