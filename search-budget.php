d21e76274ea8632be404c8f827c1056c00ecbfc6
zswalker <zswalker@my.waketech.edu>
2022-11-30 13:59:35 -0500

    sql test

--- search-budget.php
+++ search-budget.php
@@ -9,7 +9,19 @@
 
 <body>
     <?php
-    print "Initial Test"
+		$conn = mysqli_connect('localhost', 'root', '', 'test');
+
+		$sql = "SELECT * FROM inventory";
+		$result = $conn->query($sql);
+		if ($result->num_rows > 0) {
+			while($row = $result->fetch_assoc()) {
+				echo "<tr><td>".$row['id']."</td><td>".$row['car_year'].
+				"</td><td>".$row['make']."</td><td>".$row['model']."</td><td>".
+				$row['car_type']."</td><td>".$row['miles']."</td><td>".
+				$row['price']."</td></tr>";
+			}
+		}
+		$conn->close();
     ?>
 </body>
 </html>
