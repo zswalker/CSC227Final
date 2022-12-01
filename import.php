730b44a66b8a22996e90ea0125040b4593a05934
zswalker <zswalker@my.waketech.edu>
2022-12-01 16:06:12 -0500

    initial test

--- import.php
+++ import.php
@@ -17,4 +17,16 @@
     $fclose($handle);
     echo "SUCCESS";
 
+
+    $sql = "SELECT * FROM inventory";
+			$result = $conn->query($sql);
+			if ($result->num_rows > 0) {
+				while($row = $result->fetch_assoc()) {
+					echo "<tr><td>".$row['id']."</td><td>".$row['car_year'].
+					"</td><td>".$row['make']."</td><td>".$row['model']."</td><td>".
+					$row['car_type']."</td><td>".$row['miles']."</td><td>".
+					$row['price']."</td></tr>";
+				}
+			}
+
 ?>
