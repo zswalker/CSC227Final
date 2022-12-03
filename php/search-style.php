76e6c45ea2ecccd737e1c22bbebf9a77ebac89ce
zswalker <zswalker@my.waketech.edu>
2022-12-03 18:35:12 -0500

    initial style search

--- php/search-style.php
+++ php/search-style.php
@@ -8,21 +8,65 @@
 </head>
 
 <body>
-	<?php
-		$dbhost = getenv("MYSQL_SERVICE_HOST");
-		$dbport = getenv("MYSQL_SERVICE_PORT");
-		$dbuser = getenv('root');
-		$rootpwd = getenv("MYSQL_ROOT_PASSWORD");
-		$dbname = getenv("MYSQL_DATABASE");
+<?php
+	// Create variables for connection
+	$dbhost = getenv("MYSQL_SERVICE_HOST");
+	$dbport = getenv("MYSQL_SERVICE_PORT");
+	$dbuser = getenv("MYSQL_USER");
+	$dbpwd = getenv("MYSQL_PASSWORD");
+	$dbname = getenv("MYSQL_DATABASE");
 
-		$conn = new mysqli($dbhost, 'root', $rootpwd, $dbname);
-		if($conn->connect_error){
-			echo "Connection error: ".mysqli_connect_error();
-		} else {
-			echo "Connection SUCCESS";
+	// Connect to database and executes main. Prints error if unsuccessful.
+	$conn = new mysqli($dbhost, $dbuser, $dbpwd, $dbname);
+	if($conn->connect_error){
+		echo "Connection error: ".mysqli_connect_error();
+	} else {
+		// Tests Form variables and adds vehicle to database
+
+		if ($_SERVER["REQUEST_METHOD"] == "POST") {
+			// Create variables for testing
+			$v_style = $_POST['style'];
+			$error_message = "";
+
+			// Verify Style
+			if (empty($v_style)){
+				$error_message = "Error - Body Style not selected";
+			}
+
+			// Displays error message if necessary, else searches for style
+			if ($error_message != ""){
+				echo "<p>".$error_message."<p>";
+			} else {
+				// SQL Query to search for model, diplays error if necessary
+				$sql = "SELECT car_year, make, model, style, miles, price FROM inventory WHERE style='$v_style'";
+				$result = $conn->query($sql);
+				if (!$result) {
+					die("Could not successfully run query from $dbname: ".mysqli_error($conn));
+				}
+		
+				// Displays message if no results found
+				if (mysqli_num_rows($result) == 0) {
+					echo "No records found";
+				} else {
+					// else: Prints table of vehicles found
+					echo "<h1>Vehicles Found</h1>";
+					echo "<table border='1'><thead><tr><th>Year</th><th>Make</th>
+					<th>Model</th><th>Style</th><th>Miles</th><th>Price</th></tr></thead><tbody>"; 
+					while($row = mysqli_fetch_assoc($result)) {
+						echo("<tr><td>".$row["car_year"]."</td>
+						<td>".$row["make"]."</td><td>".$row["model"]."</td>
+						<td>".$row["style"]."</td><td>".$row["miles"]."</td><td>"
+						.number_format($row["price"], 0, ",")."</td></tr>");
+					}
+					echo("</table>");
+					echo("<h3>Thank you for using my program.</h3>");
+					echo('<br><footer><a calss="white" href="..\index.php">
+								Return to Form Entry</a></footer>');
+				}
+				$conn->close();
+			}
 		}
-		$conn->close();
-	
-    ?>
+	}
+?>
 </body>
 </html>
