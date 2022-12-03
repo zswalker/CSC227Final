5d4600ddcb3ccf3d6934a0b5e71458c4c46bf172
zswalker <zswalker@my.waketech.edu>
2022-12-03 01:29:59 -0500

    initial add_vehicle

--- html/add-vehicle.php
+++ html/add-vehicle.php
@@ -1,43 +1,41 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-	<meta charset="utf-8" />
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>Car Lot Search</title>
-	<link href="style.css" type="text/css" rel="stylesheet" />
-</head>
-
-<body>
-	<?php
-		// import index.php
-		require "index.php";
-		
-		function main($conn){
-			// Function Header
-			$sql2 = "DELETE FROM inventory";
-			$result = mysqli_query($conn, $sql2);
-			insert_vehicle($conn, 2005, 'Chevy', 'Silverado', 'Truck', 101050, 58000);
-			insert_vehicle($conn, 2008, 'Ford', 'Mustang GT', 'Coupe', 65658, 16800);
-			insert_vehicle($conn, 1980, 'Chevy', 'Corvette','Coupe', 95000, 22500);
-			insert_vehicle($conn, 2019, 'Honda', 'Civic Type R', 'Sedan', 21800, 35899);
-			insert_vehicle($conn, 2022, 'Acura', 'NSX', 'Coupe', 20, 171400);
+<?php
+	// Function for adding a vehicle to database
+	function insert_vehicle($conn, int $v_year, $v_make, $v_model, $v_type, int $v_miles, int $v_price){
+		$sql = "SELECT * FROM inventory";
+		$result = $conn->query($sql);
+		$v_id = 0;
+		if ($result->num_rows > 0) {
+			while($row = $result->fetch_assoc()) {
+				$v_id = $row['id'];
+			}
 		}
+		$v_id += 1;
+		$sql2 = "insert into inventory(id, car_year, make, model, car_type, miles, price) values
+		(".$v_id.",".$v_year.",".$v_make.",".$v_model.",".$v_type.",".$v_miles.",".$v_price.")";
+		$result = $conn->query($sql2);
+		print "Vehicle Added\n";
+	}
 
-		// Create variables for connection
-		$dbhost = getenv("MYSQL_SERVICE_HOST");
-		$dbport = getenv("MYSQL_SERVICE_PORT");
-		$dbuser = getenv("MYSQL_USER");
-		$dbpwd = getenv("MYSQL_PASSWORD");
-		$dbname = getenv("MYSQL_DATABASE");
+	// Create variables for connection
+	$dbhost = getenv("MYSQL_SERVICE_HOST");
+	$dbport = getenv("MYSQL_SERVICE_PORT");
+	$dbuser = getenv("MYSQL_USER");
+	$dbpwd = getenv("MYSQL_PASSWORD");
+	$dbname = getenv("MYSQL_DATABASE");
 
-		// Connect to database and executes main. Prints error if unsuccessful.
-		$conn = new mysqli($dbhost, $dbuser, $dbpwd, $dbname);
-		if($conn->connect_error){
-			echo "Connection error: ".mysqli_connect_error();
-		} else {
-			main($conn);
-		}
-	
-    ?>
-</body>
-</html>
+	// Create variable used in form
+	$car_year = $_POST['year'];
+	$make = $_POST['make'];
+	$model = $_POST['model'];
+	$style = $_POST['style'];
+	$miles = $_POST['miles'];
+	$price = $_POST['price'];
+
+	// Connect to database and executes main. Prints error if unsuccessful.
+	$conn = new mysqli($dbhost, $dbuser, $dbpwd, $dbname);
+	if($conn->connect_error){
+		echo "Connection error: ".mysqli_connect_error();
+	} else {
+		insert_vehicle($conn, $car_year, $make, $model, $style, $miles, $price);
+	}
+?>
