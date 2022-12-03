c75d151bb004b91652c25fda7749f6f3b2ec48d1
zswalker <zswalker@my.waketech.edu>
2022-12-02 23:06:11 -0500

    add id

--- index.php
+++ index.php
@@ -22,11 +22,11 @@
 			$sql = "create table if not exists inventory(id serial primary key, car_year YEAR not null, make varchar(30) not null, model varchar(30) not null, car_type varchar(30) not null, miles int(6) not null, price int(8) not null)";
 			$result = mysqli_query($conn, $sql);
 			$sql2 = "insert into inventory(car_year, make, model, car_type, miles, price) values
-			(2005, 'Chevy', 'Silverado', 'Truck', 101050, 58000),
-			(2008, 'Ford', 'Mustang GT', 'Coupe', 65658, 16800),
-			(1980, 'Chevy', 'Corvette','Coupe', 95000, 22500),
-			(2019, 'Honda', 'Civic Type R', 'Sedan', 21800, 35899),
-			(2022, 'Acura', 'NSX', 'Coupe', 20, 171400)";
+			(1, 2005, 'Chevy', 'Silverado', 'Truck', 101050, 58000),
+			(2, 2008, 'Ford', 'Mustang GT', 'Coupe', 65658, 16800),
+			(3, 1980, 'Chevy', 'Corvette','Coupe', 95000, 22500),
+			(4, 2019, 'Honda', 'Civic Type R', 'Sedan', 21800, 35899),
+			(5, 2022, 'Acura', 'NSX', 'Coupe', 20, 171400)";
 			$result = mysqli_query($conn, $sql2);
 			
 		}
