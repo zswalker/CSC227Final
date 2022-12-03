1e7310dad6bc08a05e537a5229f9fa38ce200da1
zswalker <zswalker@my.waketech.edu>
2022-12-03 16:46:38 -0500

    initial algorithm

--- php/search-model.php
+++ php/search-model.php
@@ -11,25 +11,31 @@
 		echo "Connection error: ".mysqli_connect_error();
 	} else {
 		$sql = "SELECT car_year, make, model, style, miles, price FROM inventory WHERE model='$v_model'";
-		$result = $conn->query($sql);
-		echo("<h1>Search Completed by Vehicle Model</h1>");
-		if (!$result) {
-			die("Could not successfully run query from $dbname: ".mysqli_error($conn));
-        }
-        if (mysqli_num_rows($result) == 0) {
-			echo("No records found with Model: $v_model");
-        } else {
-			echo("<h1></h1>");
-			echo "<table border = \"1\">";
-			echo("<table><thead><tr><th>Year</th><th>Make</th><th>Model</th><th>Style</th><th>Miles</th><th>Price</th></tr></thead><tbody>"); 
-			while($row = mysqli_fetch_assoc($result)) {
-				print ("<tr><td>".$row["car_year"]."</td><td>".$row["make"]."</td><td>".$row["model"]."</td><td>".$row["style"]."</td><td>".$row["miles"]."</td><td>"
-				.number_format($row["price"], 2, ".", ",")."</td></tr>");
-			}
-			echo "</table>";
-			echo "<h3>Thank you for using my program.</h3>";
-			echo '<br><footer><a calss="white" href="..\html\search-model.html">
-	 				Return to Form Entry</a></footer>';
-	  	}	
+		if(!mysqli_query($conn, $sql)){
+			echo "Error - ".$conn->error;
+		} else {
+			$result = $conn->query($sql);
+            if (!$result) {
+                die("Could not successfully run query from $dbname: ".mysqli_error($conn));
+            }
+            if (mysqli_num_rows($result) == 0) {
+                echo "No records found";
+            } else {
+                echo "<h1>Vehicles Found</h1>";
+                echo "<table border='1'><table><thead><tr><th>Year</th><th>Make</th>
+                <th>Model</th><th>Style</th><th>Miles</th><th>Price</th></tr></thead><tbody>"; 
+                while($row = mysqli_fetch_assoc($result)) {
+                    echo("<tr><td>".$row["car_year"]."</td>
+                    <td>".$row["make"]."</td><td>".$row["model"]."</td>
+                    <td>".$row["style"]."</td><td>".$row["miles"]."</td><td>"
+                    .number_format($row["price"], 1, ",")."</td></tr>");
+                }
+                echo("</table>");
+                echo("<h3>Thank you for using my program.</h3>");
+                echo('<br><footer><a calss="white" href="..\index.php">
+                         Return to Form Entry</a></footer>');
+            }
+            $conn->close();
+		}
 	}
 ?>
