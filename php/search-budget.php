1bad8d23583260838ec9d9ec6d9177449c016fb5
zswalker <zswalker@my.waketech.edu>
2022-12-04 16:32:54 -0500

    cleanup code

--- php/search-budget.php
+++ php/search-budget.php
@@ -20,65 +20,65 @@
 	$conn = new mysqli($dbhost, $dbuser, $dbpwd, $dbname);
 	if($conn->connect_error){
 		echo "Connection error: ".mysqli_connect_error();
-	} else {
-		// Tests Form variables and adds vehicle to database
-
-		if ($_SERVER["REQUEST_METHOD"] == "POST") {
-			// Create variables for testing
-			$price_low = $_POST['low'];
-			$price_high = $_POST['high'];
-			$error_message = "";
-			
-			// Verify price entries
-			if ($price_low < 0){
-				$error_message = "Error - Price can not be nagative.";
-			}
-			if ($price_high < 0){
-				$error_message = "Error - Price can not be nagative.";
-			}
-			if ($price_low > $price_high){
-				$error_message = "Error - Lower price can not be larger than Higher price.";
-			}
-			if (empty($price_low)){
-				$price_low = 0;
-			}
-			if (empty($price_high)){
-				$price_high = 10000000;
-			}
-
-			// Displays error message if necessary, else searches for price range
-			if ($error_message != ""){
-				echo "<p>".$error_message."<p>";
-			} else {
-				// SQL Query to search for model, diplays error if necessary
-				$sql = "SELECT car_year, make, model, style, miles, price FROM inventory 
-						WHERE price>'$price_low' && price<'$price_high'";
-				$result = $conn->query($sql);
-				if (!$result) {
-					die("Could not successfully run query from $dbname: ".mysqli_error($conn));
-				}
+		exit();
+	}
+	
+	// Tests Form variables and adds vehicle to database
+	if ($_SERVER["REQUEST_METHOD"] == "POST") {
+		// Create variables for testing
+		$price_low = $_POST['low'];
+		$price_high = $_POST['high'];
+		$error_message = "";
 		
-				// Displays message if no results found
-				if (mysqli_num_rows($result) == 0) {
-					echo "No records found";
-				} else {
-					// else: Prints table of vehicles found
-					echo "<h1>Vehicles Found</h1>";
-					echo "<table border='1'><thead><tr><th>Year</th><th>Make</th>
-					<th>Model</th><th>Style</th><th>Miles</th><th>Price</th></tr></thead><tbody>"; 
-					while($row = mysqli_fetch_assoc($result)) {
-						echo("<tr><td>".$row["car_year"]."</td>
-						<td>".$row["make"]."</td><td>".$row["model"]."</td>
-						<td>".$row["style"]."</td><td>".$row["miles"]."</td><td>"
-						.number_format($row["price"], 0, ",")."</td></tr>");
-					}
-					echo("</table>");
-					echo("<h3>Thank you for using my program.</h3>");
-					echo('<br><footer><a calss="white" href="..\index.php">
-								Return to Form Entry</a></footer>');
-				}
-				$conn->close();
+		// Verify price entries
+		if ($price_low < 0){
+			$error_message = "Error - Price can not be nagative.";
+		}
+		if ($price_high < 0){
+			$error_message = "Error - Price can not be nagative.";
+		}
+		if ($price_low > $price_high){
+			$error_message = "Error - Lower price can not be larger than Higher price.";
+		}
+		if (empty($price_low)){
+			$price_low = 0;
+		}
+		if (empty($price_high)){
+			$price_high = 10000000;
+		}
+
+		// Displays error message if necessary, else searches for price range
+		if ($error_message != ""){
+			echo "<p>".$error_message."<p>";
+		} else {
+			// SQL Query to search for model, diplays error if necessary
+			$sql = "SELECT car_year, make, model, style, miles, price FROM inventory 
+					WHERE price>'$price_low' && price<'$price_high'";
+			$result = $conn->query($sql);
+			if (!$result) {
+				die("Could not successfully run query from $dbname: ".mysqli_error($conn));
 			}
+	
+			// Displays message if no results found
+			if (mysqli_num_rows($result) == 0) {
+				echo "No records found";
+			} else {
+				// else: Prints table of vehicles found
+				echo "<h1>Vehicles Found</h1>";
+				echo "<table border='1'><thead><tr><th>Year</th><th>Make</th>
+				<th>Model</th><th>Style</th><th>Miles</th><th>Price</th></tr></thead><tbody>"; 
+				while($row = mysqli_fetch_assoc($result)) {
+					echo("<tr><td>".$row["car_year"]."</td>
+					<td>".$row["make"]."</td><td>".$row["model"]."</td>
+					<td>".$row["style"]."</td><td>".$row["miles"]."</td><td>"
+					.number_format($row["price"], 0, ",")."</td></tr>");
+				}
+				echo("</table>");
+				echo("<h3>Thank you for using my program.</h3>");
+				echo('<br><footer><a calss="white" href="..\index.php">
+							Return to Form Entry</a></footer>');
+			}
+			$conn->close();
 		}
 	}
 ?>
