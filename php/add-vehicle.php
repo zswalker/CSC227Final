e16f3de7d33b9f0fad735f0f974cf964d081d1cd
zswalker <zswalker@my.waketech.edu>
2022-12-04 17:04:47 -0500

    add html

--- php/add-vehicle.php
+++ php/add-vehicle.php
@@ -1,3 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8" />
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Car Lot Modify</title>
+	<link href="style.css" type="text/css" rel="stylesheet" />
+</head>
+
+<body>
 <?php
 	// Create variables for connection
 	$dbhost = getenv("MYSQL_SERVICE_HOST");
@@ -70,3 +80,5 @@
 		}
 	}
 ?>
+</body>
+</html>
