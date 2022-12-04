841c24a19eacb3877c8f72cf1c160f2e85be9f02
zswalker <zswalker@my.waketech.edu>
2022-12-04 16:44:44 -0500

    fix sql statement

--- php/search-model.php
+++ php/search-model.php
@@ -50,7 +50,7 @@
 		} else {
 			// SQL Query to search for model, diplays error if necessary
 			$sql = "SELECT car_year, make, model, style, miles, price 
-					FROM inventory WHERE style='$v_style'";
+					FROM inventory WHERE car_year='$v_year' && make='$v_make' && model='$v_model'";
 			$result = $conn->query($sql);
 			if (!$result) {
 				die("Could not successfully run query from $dbname: ".mysqli_error($conn));
