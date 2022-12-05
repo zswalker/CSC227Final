3a9816352a02dcae280b76411184ae107a97c623
zswalker <zswalker@my.waketech.edu>
2022-12-04 19:04:57 -0500

    code cleanup

--- php/search-budget.php
+++ php/search-budget.php
@@ -65,24 +65,24 @@
 					echo "No vehicles less than $".number_format($price_high, 0, ",");
 				} else {
 					echo "No vehicles found between $".number_format($price_low, 0, ",").
-					"$".number_format($price_high, 0, ",");
+						"$".number_format($price_high, 0, ",");
 				}
 			} else {
 				// else: Prints table of vehicles found
 				echo "<h1>Vehicles Found</h1>";
 				echo "<table border='1'><thead><tr><th>Year</th><th>Make</th>
-				<th>Model</th><th>Style</th><th>Miles</th><th>Price</th></tr></thead><tbody>"; 
+					<th>Model</th><th>Style</th><th>Miles</th><th>Price</th></tr></thead><tbody>"; 
 				while($row = mysqli_fetch_assoc($result)) {
-					echo("<tr><td>".$row["car_year"]."</td>
-					<td>".$row["make"]."</td><td>".$row["model"]."</td>
-					<td>".$row["style"]."</td><td>"
-					.number_format($row["miles"], 0, ",")."</td><td>"
-					.number_format($row["price"], 0, ",")."</td></tr>");
+					echo "<tr><td>".$row["car_year"]."</td>
+						<td>".$row["make"]."</td><td>".$row["model"]."</td>
+						<td>".$row["style"]."</td><td>"
+						.number_format($row["miles"], 0, ",")."</td><td>"
+						.number_format($row["price"], 0, ",")."</td></tr>";
 				}
-				echo("</table>");
-				echo("<h3>Thank you for using my program.</h3>");
-				echo('<br><footer><a calss="white" href="..\index.php">
-							Return to Form Entry</a></footer>');
+				echo "</table>";
+				echo "<h3>Thank you for using my program.</h3>";
+				echo '<br><footer><a calss="white" href="..\index.php">
+					Return to Form Entry</a></footer>';
 			}
 			$conn->close();
 		}
