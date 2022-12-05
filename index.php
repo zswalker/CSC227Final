6d5b70408b878179d7f97dfc36d7a5c7a91fc29b
zswalker <zswalker@my.waketech.edu>
2022-12-04 22:53:48 -0500

    change around info

--- index.php
+++ index.php
@@ -34,43 +34,27 @@
 		<h1>Walker's Used Car Lot</h1>
 	</header>
 	<main>
-		<div id="about">
-			<nav>
-				<img src="images\cars.jpg" alt="Cars Image" style="width:450px;height:350px;">
+		<section id="tools">
+			<img src="images\cars.jpg" alt="Cars Image" style="width:450px;height:350px;">
+			<h2>Search</h2>
+			<ul>
+				<li><a href="html\search-model.html">Search By Year/Make/Model</a></li>
+				<li><a href="html\search-budget.html">Search By Price</a></li>
+				<li><a href="html\search-style.html">Search By Style</a></li>
+				<li><a href="php\view-inventory.php">View All Vehicles</a></li>
+			</ul>
+			<br>
+			<h2>Modify</h2>
+			<ul>
+				<li><a href="html\add-vehicle.html">Add Vehicle</a></li>
+				<li><a href="html\sell-remove.html">Sell/Remove Vehicle</a></li>
+			</ul>
+			
+		</section>
+		<div id="about">				
 				<h2>About</h2>
 				<p>This website is used for finding your next used car from our used car lot.</p>
-				<br />
-			</nav>
 		</div>
-		<section id="tools">
-			<table>
-				<thead>
-					<tr>
-						<th>Search</th>
-						<th>Modify</th>
-					</tr>
-				</thead>
-				<tbody>
-				<tr>
-					<td>
-						<ul>
-							<li><a href="html\search-model.html">Search By Year/Make/Model</a></li>
-							<li><a href="html\search-budget.html">Search By Price</a></li>
-							<li><a href="html\search-style.html">Search By Style</a></li>
-							<br>
-							<li><a href="php\view-inventory.php">View All Vehicles</a></li>
-						</ul>
-					</td>
-					<td>
-						<ul>
-							<li><a href="html\add-vehicle.html">Add Vehicle</a></li>
-							<li><a href="html\sell-remove.html">Sell/Remove Vehicle</a></li>
-						</ul>
-					</td>
-				</tr>
-				</tbody>
-			</table>
-		</section>
 	</main>
 </body>
 </html>
