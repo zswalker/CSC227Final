eb47b9c039146647cf568393f8c7a3a2f2f2a7f8
zswalker <zswalker@my.waketech.edu>
2022-12-04 22:01:27 -0500

    fix output

--- php/search-model.php
+++ php/search-model.php
@@ -55,7 +55,7 @@
 	
 			// Displays message if no results found
 			if (mysqli_num_rows($result) == 0) {
-				echo "Vehicle '".$v_year." ".$v_make." ".$v_model."' not in inventory";
+				echo "<p>Vehicle with ".$v_make." make and ".$v_model." model not in inventory</p>";
 			} else {
 				// else: Prints table of vehicles found
 				echo "<h1>Vehicles Found</h1>";
