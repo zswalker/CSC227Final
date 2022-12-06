545e61a51b91176e2fce75cc68fc95319ff2d73e
zswalker <zswalker@my.waketech.edu>
2022-12-06 15:55:53 -0500

    update v id

--- php/remove-vehicle.php
+++ php/remove-vehicle.php
@@ -36,14 +36,24 @@
 		if (empty($v_id)){
 			$error_message = "<h3>Error - Vehicle ID not entered</h3>";
 		}
+		$sql = "SELECT * FROM inventory WHERE id='$v_id'";
+		$result = mysqli_query($conn, $sql);
+		if (!$result){
+			echo "<h3>Error - ".$conn->error."</h3>";
+		} else {
+			if (mysqli_num_rows($result) == 0) {
+				echo "<h3>Vehicle ID '".$v_id."' is not in inventory</h3>";
+				exit();
+			}
+		}
 
         // Display error message if necessary, else remove vehicle
 		if ($error_message != ""){
 			echo "<h3>".$error_message."</h3>";
 		} else {
             // Removes vehicle from database, displays error if necessary
-			$sql = "DELETE FROM inventory WHERE id='$v_id'";
-			if(!mysqli_query($conn, $sql)){
+			$sql2 = "DELETE FROM inventory WHERE id='$v_id'";
+			if(!mysqli_query($conn, $sql2)){
 				echo "<h3>Error - ".$conn->error."</h3>";
 			} else {
 				echo "<h4>Vehicle Successfully Removed!</h4>";
