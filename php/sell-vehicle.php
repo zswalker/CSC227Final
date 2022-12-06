5f63898f93e6c023731e7612bf17648773847d2d
zswalker <zswalker@my.waketech.edu>
2022-12-05 21:27:10 -0500

    add sell price

--- php/sell-vehicle.php
+++ php/sell-vehicle.php
@@ -29,11 +29,21 @@
 	if ($_SERVER["REQUEST_METHOD"] == "POST") {
 		// Create variable used in form
 		$v_id = $_POST['car_id'];
+		$v_price = $_POST['price'];
 		$error_message = "";
 
 		// Verify vehicle ID
 		if (empty($v_id)){
 			$error_message = "<h3>Error - Vehicle ID not entered</h3>";
+		}
+
+		// Verify sold price
+		if (empty($v_price) || $v_price < 0 || $v_price > 200000){
+			if (empty($v_price)){
+				$error_message = "<h3>Error - Vehicle price not entered</h3>";
+			} else {
+				$error_message = "<h3>Error - Vehicle price not between $0 and $200,000</h3>";
+			}
 		}
         
         // Display error message if necessary, else sets vehicle to sold
@@ -41,11 +51,11 @@
 			echo "<h3>".$error_message."</h3>";
 		} else {
             // Sets vehicle to sold in database, displays error if necessary
-			$sql = "UPDATE inventory SET sold='Sold' WHERE id='$v_id'";
+			$sql = "UPDATE inventory SET sold='Sold', sold_price='$v_price' WHERE id='$v_id'";
 			if(!mysqli_query($conn, $sql)){
 				echo "<h3>Error - ".$conn->error."</h3>";
 			} else {
-				echo "<h4>Vehicle Sold!</h4><br>";
+				echo "<h4>Vehicle Sold!</h4><br />";
 			}
 			echo '<br /><a href="..\index.php">Return to Home Page</a>';
 			$conn->close();
